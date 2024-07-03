79e6c1766f3c1328e2b0739423f80fb4fcd24449
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-07-04 04:42:40 +0530

    forward and backward propogation

---
+++ app.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+X_train = np.loadtxt('./train_X.csv', delimiter=',').T
+Y_train = np.loadtxt('./train_label.csv', delimiter=',').T
+
+X_test = np.loadtxt('./test_X.csv', delimiter = ',').T
+Y_test = np.loadtxt('./test_label.csv', delimiter = ',').T
+
+# print("shape of X_train :", X_train.shape)
+# print("shape of Y_train :", Y_train.shape)
+# print("shape of X_test :", X_test.shape)
+# print("shape of Y_test :", Y_test.shape)
+
+index = random.randrange(0, X_train.shape[1])
+plt.imshow(X_train[:, index].reshape(28, 28), cmap = 'gray')
+plt.show()
+
+def tanh(x):
+    return np.tanh(x)
+
+def relu(x):
+    return max(x, 0)
+
+def softmax(x):
+    expX = np.exp(x)
+    return expX/np.sum(expX)
+
+def derivative_tanh(x):
+    return (1 - np.power(x,2))
+
+def deriavtive_relu(x):
+    return np.array(x > 0, dtype = np.float32)
+
+def initialize_parameters(n_x, n_h,n_y):
+    w1 = np.random.randn(n_h,n_x)*0.01
+    b1 = np.zeros((n_h,1))
+
+    w2 = np.random.randn(n_y,n_h)*0.01
+    b2 = np.zeros((n_y,1))
+
+    parameters = {
+        "w1" : w1,
+        "b1" : b1,
+        "w2" : w2,
+        "b2" : b2
+    }
+    return parameters
+
+def forward_propogation(x, parameters):
+
+    w1 = parameters["w1"]
+    b1 = parameters["b1"]
+    w2 = parameters["w2"]
+    b2 = parameters["b2"]
+
+    z1 = np.dot(w1, x) + b1
+    a1 = relu(z1)
+
+    z2 = np.dot(w2, a1) + b2
+    a2 = softmax(z2)
+
+    forward_cache = {
+        "z1" : z1,
+        "a1" : a1,
+        "z2" : z2,
+        "a2" : a2
+    }
+
+    return forward_cache
+
+def cost_function(a2, y):
+    m = y.shape[1]
+    
+    cost = -(1/m)*np.sum(y*np.log(a2))
+    
+    #cost = -(1/m)*np.sum(np.sum(y*np.log(a2, 0), 1))
+    
+    return cost
+
+def backward_prop(x, y, parameters, forward_cache):
+    
+    w1 = parameters['w1']
+    b1 = parameters['b1']
+    w2 = parameters['w2']
+    b2 = parameters['b2']
+    
+    a1 = forward_cache['a1']
+    a2 = forward_cache['a2']
+    
+    m = x.shape[1] #Total number of observations
+    
+    dz2 = (a2 - y)
+    dw2 = (1/m)*np.dot(dz2, a1.T)
+    db2 = (1/m)*np.sum(dz2, axis = 1, keepdims = True)
+    
+    dz1 = (1/m)*np.dot(w2.T, dz2)*derivative_tanh(a1)
+    dw1 = (1/m)*np.dot(dz1, x.T)
+    db1 = (1/m)*np.sum(dz1, axis = 1, keepdims = True)
+    
+    gradients = {
+        "dw1" : dw1,
+        "db1" : db1,
+        "dw2" : dw2,
+        "db2" : db2
+    }
+    
+    return gradients
+
+
